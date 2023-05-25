157e501a30a82495d9e2c788938ed22401426d3e
NadSafwat <98837821+NadSafwat@users.noreply.github.com>
2023-05-25 11:57:34 +0300

    added module description

--- synchronizer.v
+++ synchronizer.v
@@ -1,6 +1,6 @@
 // file: synchronizer.v
 // author: Nour Kasaby, Nadine Safwat, Mariam ElGhobary
-// This module will 
+// This module will reduce the risk of metastability by filtering the mechanical input through an always block (D-flip flop)
 
 `timescale 1ns/1ns
 
